35bb922a860f4abbae457a80b19134727cc0bd6e
himanshubhadoria2727 <hbhadoria78@gmail.com>
2026-03-20 13:07:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+﻿import React, { useMemo } from "react";
+
+export default function App() {
+  const frameSrc = useMemo(() => "/legacy-index.html", []);
+
+  return (
+    <iframe
+      title="Digital Menu Legacy"
+      src={frameSrc}
+      style={{ width: "100%", height: "100vh", border: "0", display: "block" }}
+    />
+  );
+}
